eade8a42b9ef67b9fbf525c4dc6f9e98ea2d868e
VadimKurachynski <kurachynski@gmail.com>
2023-01-09 23:02:32 +0300

    56

--- features/themes/themesSlice.js
+++ features/themes/themesSlice.js
@@ -5,32 +5,29 @@ const initialState = {
     themes: []
 }
 
-
-export const getThemes = createAsyncThunk('themes/getThemes', async (_, {rejectWithValue,dispatch}) => {
-    const res=await axios.get('https://jsonplaceholder.typicode.com/posts')
+export const getThemes = createAsyncThunk('themes/getThemes', async (_, {rejectWithValue, dispatch}) => {
+    const res = await axios.get('http://localhost:5000/api/themesname',{withCredentials:true})
     dispatch(setThemes(res.data))
 })
-
 
 export const themesSlice = createSlice({
     name: 'themes',
     initialState,
     reducers: {
-        setThemes:(state,action)=>{
-            state.themes=action.payload
+        setThemes: (state, action) => {
+            state.themes = action.payload
         },
-
     },
-    extraReducers:(builder)=> {
-        builder
-            .addCase(getThemes.pending,()=> console.log('getThemes: pending'))
-            .addCase(getThemes.fulfilled,()=> console.log('getThemes: fulfilled'))
-            .addCase(getThemes.rejected,()=> console.log('getThemes: rejected'))
-             //pending : Вызывается прямо перед выполнением запроса
-            //fulfilled : Вызывается в том случае если запрос успешно выполнился
-            //rejected : Вызывается в случае ошибки
-         }
+    // extraReducers: (builder) => {
+    //     builder
+    //         .addCase(getThemes.pending, () => console.log('getThemes: pending'))
+    //         .addCase(getThemes.fulfilled, () => console.log('getThemes: fulfilled'))
+    //         .addCase(getThemes.rejected, () => console.log('getThemes: rejected'))
+    //     //pending : Вызывается прямо перед выполнением запроса
+    //     //fulfilled : Вызывается в том случае если запрос успешно выполнился
+    //     //rejected : Вызывается в случае ошибки
+    // }
 })
 
-export const {setThemes}=themesSlice.actions
+export const {setThemes} = themesSlice.actions
 export default themesSlice.reducer
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,48 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import {getThemes} from "../features/themes/themesSlice";
-import {useDispatch} from "react-redux";
-
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import s from "../styles/styleMe.module.css"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const dispatch=useDispatch()
+    const dispatch=useDispatch()
+
+    useEffect(() => {
+        dispatch(getThemes());
+    },[]);
+
+
+
+    const themes=useSelector((state)=>state.themes.themes)
+    console.log(themes)
 
 
   return (
     <>
-<div>
-  <button onClick={()=>dispatch(getThemes())} >НАЖАТЬ</button>
-</div>
+        <button onClick={()=> dispatch(getThemes())}>нажми</button>
+
+     {themes.map((theme)=>(
+         <span key={theme.themenumber}>
+           <div className={s.title}>Тема {theme.themenumber<9?'0'+theme.themenumber:theme.themenumber}</div>
+             <div className={s.title}>Название: {theme.themename}</div>
+             <div className={s.title}>Кол-во вопросов: {theme.numberofquestions}</div>
+         </span>  ))}
+
+
     </>
-  )
+
+     )
+
+
+
+
 }
+
+
+
+
+
+
