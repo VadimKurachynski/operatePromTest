a66862d9e066a0930d6e0d4509ff333d51c1766e
VadimKurachynski <kurachynski@gmail.com>
2023-01-10 22:57:43 +0300

    whwv

--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,7 @@
-//import "../styles/new.module.css";
-//import "../styles/globals.css"
 import s from "../styles/test.module.css";
+import React from 'react';
+
+
 
 
 
@@ -9,34 +10,41 @@ function handleClick(e) {
     console.log(e);
 }
 
-
-const Der=()=>{
-
-    const text="Лица, ответственные за безопасную эксплуатацию объектов " +
+const Der = () => {
+    const tema = "Тема 999: Вопросов 1340";
+    const text = "Лица, ответственные за безопасную эксплуатацию объектов " +
         "газораспределительной системы и газопотребления = Лица, осуществляющие " +
         " и являющиеся ответственными за ведение работ при выполнении работ и (или)" +
         " оказании услуг по обслуживанию, ремонту объектов " +
         "газораспределительной системы и газопотребления"
-
-
-    return(
+    return (
         <>
-            <div className={s.myflexcont}>
-                <div className={s.myflexbox}>Тема 01: Вопросов 340</div>
-                <div className={s.myflexbox}>{text}</div>
+            <style jsx global>
+                {`body {
+                  font-family: sans-serif;
+                  color: green;
+                  background: #5d90a6;
+                  //height: 100vh;
+                } `}</style>
+
+            <div className={s.wrapper}>
+                <div className={s.header}>header</div>
+                <div className={s.content}>
+                    <div className={s.container}>
+                        <div className={s.myflexcont}>
+                            <div className={s.myflexbox}>{tema}</div>
+                            <div className={s.myflexbox}>{text}</div>
+                        </div>
+                        <div className={s.myflexcont}>
+                            <div className={s.myflexbox}>{tema}</div>
+                            <div className={s.myflexbox}>{text}</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={s.footer}>footer</div>
             </div>
-            <style jsx global>{`
-        body {
-          font-family: sans-serif;
-          background-color: green;
-        }
-      `}</style>
-
-
-
         </>
     )
-
 }
-
 export default Der;
