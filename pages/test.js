3a30698c330eab460b05d02ae5d2dc0d7ab770dd
VadimKurachynski <kurachynski@gmail.com>
2023-01-11 11:54:19 +0300

    main

--- pages/test.js
+++ pages/test.js
@@ -1,18 +1,32 @@
 import s from "../styles/test.module.css";
 import React from 'react';
 import {Button} from "antd";
-
-
-
-
+import Head from 'next/head'
+import {Roboto} from '@next/font/google'
+import React from 'react';
+import { Switch } from 'antd';
+const roboto = Roboto({
+    weight: '400',
+    subsets: ['latin'],
+})
 
 function handleClick(e) {
     e.preventDefault();
     console.log(e);
 }
+const onChange = (checked) => {
+    console.log(`switch to ${checked}`);
+};
+
+function print(g1, g2) {
+    //e.preventDefault();
+    console.log(g1, g2);
+}
 
 const Der = () => {
     const tema = "Тема 999: Вопросов 1340";
+    const theme = "999";
+    const questions = "1340";
     const text = "Лица, ответственные за безопасную эксплуатацию объектов " +
         "газораспределительной системы и газопотребления = Лица, осуществляющие " +
         " и являющиеся ответственными за ведение работ при выполнении работ и (или)" +
@@ -20,53 +34,40 @@ const Der = () => {
         "газораспределительной системы и газопотребления"
     return (
         <>
+            <div className={roboto.className}>
+            <Head>
+                <title>My page title</title>
+
+
+            </Head>
             <style jsx global>
                 {`body {
-                  font-family: sans-serif;
                   color: #d27941;
                   background: #81c4ff;
-                  //height: 100vh;
-                } `}</style>
+                }
+                `}</style>
 
             <div className={s.wrapper}>
 
                 <div className={s.header}>header</div>
-                    <div className={s.container}>
-                <div className={s.content}>
 
-                      <a href={"#rerr"}>  <div className={s.myflexcont}>
+                <div className={s.content}>
+                    <div className={s.container}>
+
+                        <div onClick={() => print(`${theme}`, `${questions}`)} className={s.myflexcont}>
                             <div className={s.myflexbox}>{tema}</div>
                             <div className={s.myflexbox}>{text}</div>
-                        </div></a>
-                    <a href={"#rerr"}>  <div className={s.myflexcont}>
-                        <div className={s.myflexbox}>{tema}</div>
-                        <div className={s.myflexbox}>{text}</div>
-                    </div></a>
-                    <a href={"#rerr"}>  <div className={s.myflexcont}>
-                        <div className={s.myflexbox}>{tema}</div>
-                        <div className={s.myflexbox}>{text}</div>
-                    </div></a>
-                    <a href={"#rerr"}>  <div className={s.myflexcont}>
-                        <div className={s.myflexbox}>{tema}</div>
-                        <div className={s.myflexbox}>{text}</div>
-                    </div></a>
-                    <a href={"#rerr"}>  <div className={s.myflexcont}>
-                        <div className={s.myflexbox}>{tema}</div>
-                        <div className={s.myflexbox}>{text}</div>
-                    </div></a>
-                    <a href={"#rerr"}>  <div className={s.myflexcont}>
-                        <div className={s.myflexbox}>{tema}</div>
-                        <div className={s.myflexbox}>{text}</div>
-                    </div></a>
-                    <a href={"#rerr"}>  <div className={s.myflexcont}>
-                        <div className={s.myflexbox}>{tema}</div>
-                        <div className={s.myflexbox}>{text}</div>
-                    </div></a>
+                            <div className={s.myflexbox}>
 
-                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
                 <div className={s.footer}>footer</div>
 
+            </div>
             </div>
         </>
     )
