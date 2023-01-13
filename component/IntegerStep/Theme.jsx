74168d2f206803e658125390de79c464be17da0b
VadimKurachynski <kurachynski@gmail.com>
2023-01-13 14:38:34 +0300

    thems

--- component/IntegerStep/Theme.jsx
+++ component/IntegerStep/Theme.jsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import s from "../../styles/test.module.css";
 import {CheckOutlined, CloseOutlined} from "@ant-design/icons";
 
-const IntegerStep = () => {
+const Theme = () => {
     const [inputValueOne, setInputValueOne] = useState(1);
     const [inputValueTwo, setInputValueTwo] = useState(100);
     const [disabled, setDisabled] = useState(false);
@@ -23,23 +23,40 @@ const IntegerStep = () => {
         setInputValueTwo(newValue);
     };
 
+    function print(g1, g2) {
+
+        console.log(g1, g2);
+    }
+
+
+    const tema = "Тема 999: Вопросов 1340";
+    const theme = "999";
+    const questions = "1340";
+    const text = "Лица, ответственные за безопасную эксплуатацию объектов " +
+        "газораспределительной системы и газопотребления = Лица, осуществляющие " +
+        " и являющиеся ответственными за ведение работ при выполнении работ и (или)" +
+        " оказании услуг по обслуживанию, ремонту объектов " +
+        "газораспределительной системы и газопотребления"
+
 
     return (
         <>
+            <div onClick={() => print(`${theme}`, `${questions}`)} className={s.myflexcont}>
+                <div className={s.myflexboxName}>{tema}</div>
+                <div className={s.myflexboxText}>{text}</div>
+            </div>
 
+            <div className={s.myButtonFlex}>
             <div className={s.allBlock}>
-
                 <div className={s.nameBloсk}>
                     <div className={s.item}>Перемешать вопросы:</div>
                     <div className={s.item}>Перемешивать ответы:</div>
                     <div className={s.item}>Выбрать диапазон вопросов:</div>
                 </div>
-
-
                 <div className={s.switchBlock}>
                     <div className={s.item}><Switch checkedChildren={<CheckOutlined/>}
                                                     unCheckedChildren={<CloseOutlined/>}
-                                                    defaultChecked/></div>
+                                                    defaultChecked={false}/></div>
                     <div className={s.item}><Switch checkedChildren={<CheckOutlined/>}
                                                     unCheckedChildren={<CloseOutlined/>}
                                                     defaultChecked/></div>
@@ -52,9 +69,7 @@ const IntegerStep = () => {
             </div>
 
 
-
-
-            <div >
+            <div style={{display: disabled ? "block" : "none"}}>
                 <div className={s.slider}>
                     <Slider range defaultValue={[0, 100]}
                             disabled={!disabled}
@@ -91,9 +106,11 @@ const IntegerStep = () => {
                     </div>
                 </div>
             </div>
+
+            </div>
         </>
     )
 };
 
 
-export default IntegerStep;
+export default Theme;
--- pages/test.js
+++ pages/test.js
@@ -6,7 +6,7 @@ import {Roboto} from '@next/font/google'
 import {Slider, Switch,} from 'antd';
 import {Col, InputNumber, Row} from 'antd';
 import {CheckOutlined, CloseOutlined} from '@ant-design/icons';
-import IntegerStep from "../component/IntegerStep/IntegerStep";
+import Theme from "../component/IntegerStep/Theme";
 
 
 const roboto = Roboto({
@@ -14,56 +14,20 @@ const roboto = Roboto({
     subsets: ['latin'],
 })
 
-function handleClick(e) {
-    e.preventDefault();
-    console.log(e);
-}
-
-const onChange = (checked) => {
-    console.log(`switch to ${checked}`);
-};
-
-function print(g1, g2) {
-    //e.preventDefault();
-    console.log(g1, g2);
-}
-
 
 const Der = () => {
 
-    const [inputValue, setInputValue] = useState(1);
-    const onChange = (newValue) => {
-        setInputValue(newValue);
-    };
-
-
-    const tema = "Тема 999: Вопросов 1340";
-    const theme = "999";
-    const questions = "1340";
-    const text = "Лица, ответственные за безопасную эксплуатацию объектов " +
-        "газораспределительной системы и газопотребления = Лица, осуществляющие " +
-        " и являющиеся ответственными за ведение работ при выполнении работ и (или)" +
-        " оказании услуг по обслуживанию, ремонту объектов " +
-        "газораспределительной системы и газопотребления"
     return (
         <>
             <div className={roboto.className}>
                 <Head>
                     <title>My page title</title>
                 </Head>
-
-
                 <div className={s.wrapper}>
                     <div className={s.header}>header</div>
                     <div className={s.content}>
                         <div className={s.container}>
-                            <div onClick={() => print(`${theme}`, `${questions}`)} className={s.myflexcont}>
-                                <div className={s.myflexboxName}>{tema}</div>
-                                <div className={s.myflexboxText}>{text}</div>
-                            </div>
-                            <div className={s.myButtonFlex}>
-                                <IntegerStep/>
-                            </div>
+                            <Theme/>
                         </div>
                     </div>
                     <div className={s.footer}>footer</div>
