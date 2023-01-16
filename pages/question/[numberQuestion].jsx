914e9b3d2f0aab48dd39f75d265d70d2375a6131
VadimKurachynski <kurachynski@gmail.com>
2023-01-16 19:58:38 +0300

    34

---
+++ pages/question/[numberQuestion].jsx
@@ -0,0 +1,21 @@
+import {useDispatch, useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {getThemes} from "../../features/themes/themesSlice";
+import Head from "next/head";
+import s from "../../styles/test.module.css";
+import Theme from "../../component/IntegerStep/Theme";
+
+
+const QuestionPage = () => {
+
+
+
+
+
+    return (
+        <>
+           <div>динамик</div>
+        </>
+    )
+}
+export default QuestionPage;
