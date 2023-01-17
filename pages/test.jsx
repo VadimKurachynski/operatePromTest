03baf3e46ea5030f48aa763ef2c88ce0309be9c4
VadimKurachynski <kurachynski@gmail.com>
2023-01-17 23:44:57 +0300

    df

--- pages/test.jsx
+++ pages/test.jsx
@@ -6,15 +6,18 @@ import {useSelector} from "react-redux";
 
 const PageTest = () => {
     const {themes, selectTheme,questionsSelectTheme} = useSelector((state) => state.themes)
-     console.log(questionsSelectTheme)
+     // console.log(questionsSelectTheme)
     // console.log(selectTheme)
     // console.log(themes)
+   questionsSelectTheme.sort((x, y) => x.nomvoprosa - y.nomvoprosa);
+
     return (
         <>
+
                             <div>САМ ТЕСТ</div>
             <div>
-                { questionsSelectTheme.reverse().map((qu)=>(
-                    debug
+                { questionsSelectTheme2.map((qu)=>(
+
                     <div key={qu.nomvoprosa}>
                         {qu.nomvoprosa}</div>
                 ))
