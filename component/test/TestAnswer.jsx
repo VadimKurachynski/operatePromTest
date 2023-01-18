175870aaaacf19dcf84b5220d965461778e8ff17
VadimKurachynski <kurachynski@gmail.com>
2023-01-18 17:26:37 +0300

    d

---
+++ component/test/TestAnswer.jsx
@@ -0,0 +1,44 @@
+
+import s from "../test/testAnswer.module.css";
+import { useState } from 'react';
+import {useSelector} from "react-redux";
+import { Input, Radio, Space } from 'antd';
+
+const TestAnswer = () => {
+    const {themes, selectTheme,questionsSelectTheme} = useSelector((state) => state.themes)
+    const [value, setValue] = useState(1);
+    const onChange = (e) => {
+        console.log('radio checked', e.target.value);
+        setValue(e.target.value);
+    };
+
+    const arraySort = [...questionsSelectTheme]
+    arraySort.sort((x, y) => x.nomvoprosa - y.nomvoprosa);
+    arraySort.sort(() => 0.5-Math.random());
+
+    return(
+<>
+
+        <div>Answer</div>
+
+    <Radio.Group style={{
+
+    }}onChange={onChange} >
+        <Space direction="vertical">
+          <div className={s.radio}> <Radio value={1}><span className={s.dd}> отметка (ставится штамп) «зарегистрировано»</span></Radio></div>
+            <div className={s.radio}> <Radio value={2}>Делается отметка (ставится штамп) о регистрации потенциально
+                опасного объекта с указанием регистрирующего органа, регистрационного номера,
+                даты регистрации и фамилии, собственного имени, отчества (если таковое имеется)
+                должностного лица регистрирующего органа</Radio></div>
+                <div className={s.radio}> <Radio value={3}>Регистрационный номер потенциально опасного объекта</Radio></div>
+        </Space>
+    </Radio.Group>
+
+
+
+
+
+</>
+    )
+}
+export default TestAnswer;
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,32 +1,20 @@
 import React from "react";
-import {useSelector} from "react-redux";
 
-
+import {IssuesCloseOutlined} from "@ant-design/icons";
+import {Button} from "antd";
+import { Input, Radio, Space } from 'antd';
+import { useState } from 'react';
+import TestAnswer from "../component/test/TestAnswer";
 
 
 const PageTest = () => {
-    const {themes, selectTheme,questionsSelectTheme} = useSelector((state) => state.themes)
-     // console.log(questionsSelectTheme)
-    // console.log(selectTheme)
-    // console.log(themes)
 
-    const arraySort = [...questionsSelectTheme]
-    arraySort.sort((x, y) => x.nomvoprosa - y.nomvoprosa);
-    arraySort.sort(() => 0.5-Math.random());
+
 
     return (
         <>
 
-                            <div>САМ ТЕСТ</div>
-            <div>
-                { arraySort.map((qu)=>(
-
-                    <div key={qu.nomvoprosa}>
-                        {qu.nomvoprosa}</div>
-                ))
-                }
-            </div>
-
+            <TestAnswer />
 
 
 
