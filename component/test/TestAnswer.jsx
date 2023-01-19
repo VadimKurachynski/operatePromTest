a7b3233a0b9e28d503a46d47a3deee8daf832e30
VadimKurachynski <kurachynski@gmail.com>
2023-01-19 23:45:36 +0300

    liter

--- component/test/TestAnswer.jsx
+++ component/test/TestAnswer.jsx
@@ -3,6 +3,9 @@ import s from "../test/testAnswer.module.css";
 import { useState } from 'react';
 import {useSelector} from "react-redux";
 import { Input, Radio, Space } from 'antd';
+import { Button} from 'antd';
+import {QuestionCircleOutlined} from '@ant-design/icons';
+
 
 const TestAnswer = () => {
     const {themes, selectTheme,questionsSelectTheme} = useSelector((state) => state.themes)
@@ -19,7 +22,8 @@ const TestAnswer = () => {
     return(
 <>
 
-<div className={s.question}>Какими полномочиями наделен работник службы промышленной безопасности (инженер по промышленной безопасности,
+
+    <div className={s.question}><QuestionCircleOutlined style={{ fontSize: '24px' }}/>   Какими полномочиями наделен работник службы промышленной безопасности (инженер по промышленной безопасности,
     ответственное лицо, на которое возложены соответствующие обязанности)?</div>
 
     <Radio.Group style={{
@@ -33,6 +37,14 @@ const TestAnswer = () => {
                 <div className={s.radio}> <Radio value={3}><span className={s.radioText}>Регистрационный номер потенциально опасного объекта</span></Radio></div>
         </Space>
     </Radio.Group>
+<div className={s.correctAnswer}>Правильный ответ:
+    Проводить анализ состояния промышленной безопасности, соблюдения требований промышленной безопасности,
+    рассматривать документы по вопросам промышленной безопасности</div>
+
+<div><Button type="primary">следующий вопрос</Button></div>
+
+  <div>Литература:</div>
+    <div>Закон Республики Беларусь от 5 января 2016 г. № 354-З «О промышленной безопасности». статья 30</div>
 
 
 
