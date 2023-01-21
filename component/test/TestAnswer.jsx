261f8b551ad2850091b1d83abca948c25cf4f793
VadimKurachynski <kurachynski@gmail.com>
2023-01-21 16:37:44 +0300

    css t

--- component/test/TestAnswer.jsx
+++ component/test/TestAnswer.jsx
@@ -1,14 +1,13 @@
-
 import s from "../test/testAnswer.module.css";
-import { useState } from 'react';
+import {useState} from 'react';
 import {useSelector} from "react-redux";
-import { Input, Radio, Space } from 'antd';
-import { Button} from 'antd';
+import {Input, Radio, Space} from 'antd';
+import {Button} from 'antd';
 import {QuestionCircleOutlined} from '@ant-design/icons';
 
 
 const TestAnswer = () => {
-    const {themes, selectTheme,questionsSelectTheme} = useSelector((state) => state.themes)
+    const {themes, selectTheme, questionsSelectTheme} = useSelector((state) => state.themes)
     const [value, setValue] = useState(1);
     const onChange = (e) => {
         console.log('radio checked', e.target.value);
@@ -17,52 +16,70 @@ const TestAnswer = () => {
 
     const arraySort = [...questionsSelectTheme]
     arraySort.sort((x, y) => x.nomvoprosa - y.nomvoprosa);
-    arraySort.sort(() => 0.5-Math.random());
+    arraySort.sort(() => 0.5 - Math.random());
 
-    return(
-<>
+    return (
+        <>
+<div className={s.body}>
+            {/*<QuestionCircleOutlined style={{ fontSize: '30px' }}/>*/}
+            <div className={s.nameTheme}>Тема 01 - Председатель (заместитель председателя) комиссии для проверки знаний по вопросам промышленной
+                безопасности, созданной в субъекте промышленной безопасности
+            </div>
+            <div className={s.boxCountQuestions}>
+                <div>Вопрос № 1</div>
+                <div>Вопросов: 80</div>
+            </div>
 
-    {/*<QuestionCircleOutlined style={{ fontSize: '30px' }}/>*/}
-    <div>Тема 01 - Председатель (заместитель председателя) комиссии для проверки знаний по вопросам промышленной безопасности, созданной в субъекте промышленной безопасности</div>
-    <div className={s.boxCountQuestions}><div>Вопрос № 1</div> <div>Вопросов: 80</div></div>
 
-<div>
-    <div>кол-во пройденных вопросов</div>
-    <div>кол-во правильных ответов</div>
-    <div>кол-во ошибок</div>
-    <div>60</div>
-    <div>40</div>
-    <div>20</div>
-</div>
+            <div className={s.table}>
+                <div className={s.column}>
+                    <div className={s.tableText}>кол-во пройденных вопросов</div>
+                    <div>60</div>
+                </div>
 
-    <div className={s.question}>    <div className={s.questionText}> Какими полномочиями наделен работник службы промышленной безопасности (инженер по промышленной безопасности,
-    ответственное лицо, на которое возложены соответствующие обязанности)?</div></div>
-<div className={s.boxRadio}>
-    <Radio.Group style={{
-    }}onChange={onChange} >
-        <Space direction="vertical">
-          <div className={s.radio}> <Radio value={1}><span className={s.radioText}> отметка (ставится штамп) «зарегистрировано»</span></Radio></div>
-            <div className={s.radio}> <Radio value={2}><span className={s.radioText}>Делается отметка (ставится штамп) о регистрации потенциально
+                <div className={s.column}>
+                    <div className={s.tableText}>кол-во правильных ответов</div>
+                    <div>40</div>
+                </div>
+                <div className={s.column}>
+                    <div className={s.tableText}>кол-во ошибок</div>
+                    <div>20</div>
+                </div>
+            </div>
+
+
+            <div className={s.question}>
+                <div className={s.questionText}> Какими полномочиями наделен работник службы промышленной безопасности
+                    (инженер по промышленной безопасности,
+                    ответственное лицо, на которое возложены соответствующие обязанности)?
+                </div>
+            </div>
+            <div className={s.boxRadio}>
+                <Radio.Group style={{}} onChange={onChange}>
+                    <Space direction="vertical">
+                        <div className={s.radio}><Radio value={1}><span className={s.radioText}> отметка (ставится штамп) «зарегистрировано»</span></Radio>
+                        </div>
+                        <div className={s.radio}><Radio value={2}><span className={s.radioText}>Делается отметка (ставится штамп) о регистрации потенциально
                 опасного объекта с указанием регистрирующего органа, регистрационного номера,
                 даты регистрации и фамилии, собственного имени, отчества (если таковое имеется)
                 должностного лица регистрирующего органа</span></Radio></div>
-                <div className={s.radio}> <Radio value={3}><span className={s.radioText}>Регистрационный номер потенциально опасного объекта</span></Radio></div>
-        </Space>
-    </Radio.Group>
+                        <div className={s.radio}><Radio value={3}><span className={s.radioText}>Регистрационный номер потенциально опасного объекта</span></Radio>
+                        </div>
+                    </Space>
+                </Radio.Group>
+            </div>
+            <div className={s.correctAnswer}>Правильный ответ:
+                Проводить анализ состояния промышленной безопасности, соблюдения требований промышленной безопасности,
+                рассматривать документы по вопросам промышленной безопасности
+            </div>
+
+            <div><Button type="primary">следующий вопрос</Button></div>
+
+            <div>Литература:</div>
+            <div>Закон Республики Беларусь от 5 января 2016 г. № 354-З «О промышленной безопасности». статья 30</div>
+
 </div>
-<div className={s.correctAnswer}>Правильный ответ:
-    Проводить анализ состояния промышленной безопасности, соблюдения требований промышленной безопасности,
-    рассматривать документы по вопросам промышленной безопасности</div>
-
-<div><Button type="primary">следующий вопрос</Button></div>
-
-  <div>Литература:</div>
-    <div>Закон Республики Беларусь от 5 января 2016 г. № 354-З «О промышленной безопасности». статья 30</div>
-
-
-
-
-</>
+        </>
     )
 }
 export default TestAnswer;
