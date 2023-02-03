3edb0bc3129e1cb5518c144b1403bf657bbf116c
VadimKurachynski <kurachynski@gmail.com>
2023-02-03 20:52:28 +0300

    ref

--- component/layout/Layout.jsx
+++ component/layout/Layout.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-
-import HeadTest from "../head/HeadThemes";
 import s from "../layout/layout.module.css"
 import HeaderTest from "../header/Header";
 import FooterTest from "../footer/Footer";
 
-
-
-
-
 const Layout = ({children}) => {
     return (
         <>
-                <div className={s.wrapper}>
-                    <HeaderTest/>
-                    <div className={s.content}>
-                        <div className={s.container}>
-                            {children}
-                        </div>
+            <div className={s.wrapper}>
+                <HeaderTest/>
+                <div className={s.content}>
+                    <div className={s.container}>
+                        {children}
                     </div>
-                    <FooterTest/>
                 </div>
+                <FooterTest/>
+            </div>
         </>
     )
 }
