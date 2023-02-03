2887fb6a6d325a3e0da8e7873d6fe63cc9ca2a4a
VadimKurachynski <kurachynski@gmail.com>
2023-02-03 20:50:14 +0300

    ref

--- component/footer/Footer.jsx
+++ component/footer/Footer.jsx
@@ -1,20 +1,10 @@
-
 import React from "react";
 import s from "../footer/footer.module.css";
-
-
-
 
 const FooterTest = () => {
     return(
         <div className={s.footer}>
-
-
-
-           <div className={s.name}> @</div>
-
-
-
+           <div className={s.name}> admin@operate.su</div>
         </div>
     )
 }
