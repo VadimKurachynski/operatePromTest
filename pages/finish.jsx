c22d774ec28f3135de4addbdbb64f7ff24008016
VadimKurachynski <kurachynski@gmail.com>
2023-02-03 21:02:59 +0300

    ref

--- pages/finish.jsx
+++ pages/finish.jsx
@@ -1,86 +1,54 @@
 import {useDispatch, useSelector} from "react-redux";
-import React, {useEffect} from "react";
+import React from "react";
 import ResultTable from "../component/resultTable/ResultTable";
 import s from "../component/theme/themes.module.css";
 import {Button} from "antd";
-import {IssuesCloseOutlined} from "@ant-design/icons";
 import {setCorrectS, setNoCorrectS} from "../features/themes/themesSlice";
 import {useRouter} from "next/router";
 import Head from "next/head";
 
-
 const PageFinish = () => {
-
-    const router=useRouter()
-    const dispatch=useDispatch();
+    const router = useRouter()
+    const dispatch = useDispatch();
     const {
         selectTheme,
         questionsSelectTheme,
         selectNameTheme,
         selectQuestionsRange,
-        isLoader,
         correct,
         noCorrect
     } = useSelector((state) => state.themes)
     const lengthQuestions = questionsSelectTheme.length;
-
-    const startThemes=()=>{
+    const startThemes = () => {
         router.push('/')
         dispatch(setCorrectS(0));
         dispatch(setNoCorrectS(0));
     }
-
-
-    // useEffect(() => {
-    //     if (selectTheme===0) {
-    //         router.push('/')
-    //     }
-    // }, [])
-
-
-
-
     return (
         <>
-                <Head>
+            <Head>
                 <title>Результаты</title>
-                </Head>
-
-
+            </Head>
             {
+                (selectTheme === 0) ? <div></div>
+                    :
+                    <>
+                        <div style={{textAlign: "center", margin: "20px"}}>Вы закончили тест</div>
+                        <div style={{textAlign: "center", fontWeight: "600"}}>Тема{selectTheme}</div>
+                        <div style={{textAlign: "center"}}>{selectNameTheme}</div>
+                        <div style={{textAlign: "center", margin: "10px"}}>диапазон пройденных вопросов из
+                            темы:{selectQuestionsRange[0]}--{selectQuestionsRange[1]}</div>
+                        <ResultTable
+                            lengthQuestions={lengthQuestions}
+                            correct={correct}
+                            noCorrect={noCorrect}
+                        />
+                        <div className={s.mainButton}><Button type="primary" onClick={startThemes}>ПЕРЕЙТИ К ВЫБОРУ
+                            ТЕМЫ</Button></div>
 
-                (selectTheme===0)?<div></div>
-
-
-                :
-
-
-               <>
-
-                <div style={{textAlign: "center", margin: "20px"}}>Вы закончили тест</div>
-                <div style={{textAlign: "center", fontWeight:"600"}}>Тема{selectTheme}</div>
-                <div style={{textAlign: "center"}}>{selectNameTheme}</div>
-                <div style={{textAlign: "center",margin:"10px"}}>диапазон пройденных вопросов из темы:{selectQuestionsRange[0]}--{selectQuestionsRange[1]}</div>
-                <ResultTable
-                lengthQuestions={lengthQuestions}
-                correct={correct}
-                noCorrect={noCorrect}
-                />
-                <div  className={s.mainButton}><Button  type="primary"  onClick={startThemes}>ПЕРЕЙТИ К ВЫБОРУ ТЕМЫ</Button></div>
-
-               </>
-
-
+                    </>
             }
-
-
-            </>
-
-
-
-
-
-
+        </>
 
     )
 
--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,30 @@ import React, {useEffect} from 'react';
 import s from "../component/theme/themes.module.css";
 import Theme from "../component/theme/Theme";
 import {useDispatch, useSelector} from "react-redux";
-import {getAuth, getThemes, postAuth} from "../features/themes/themesSlice";
+import {getAuth, getThemes} from "../features/themes/themesSlice";
 import Head from "next/head";
 
-
-
 const PageThemes = () => {
-
     const dispatch = useDispatch()
-
     useEffect(() => {
         dispatch(getThemes());
         dispatch(getAuth());
-
     }, []);
-
-    const {themes, selectTheme, isLoader} = useSelector((state) => state.themes)
-
+    const {themes, selectTheme} = useSelector((state) => state.themes)
     return (
         <>
             <Head>
                 <title>Выбор темы</title>
             </Head>
-            <div className={s.head}>{(selectTheme === 0)?'Выберите тему для тренировки':'Выберите нужные настройки'}</div>
+            <div
+                className={s.head}>{(selectTheme === 0) ? 'Выберите тему для тренировки' : 'Выберите нужные настройки'}</div>
             {
-                    themes.map((theme) => (
-                        <div key={theme.themenumber}
-                             style={{display: (theme.themenumber === selectTheme || selectTheme === 0) ? "block" : "none"}}>
-                            <Theme theme={theme}/>
-                        </div>))
+                themes.map((theme) => (
+                    <div key={theme.themenumber}
+                         style={{display: (theme.themenumber === selectTheme || selectTheme === 0) ? "block" : "none"}}>
+                        <Theme theme={theme}/>
+                    </div>))
             }
-
         </>
     )
 }
